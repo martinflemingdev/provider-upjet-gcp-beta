63d56d937189972e4df595c748924c094050c94d
Martin Fleming <martinfleming@pop-os.localdomain>
2025-09-08 17:21:02 -0400

    configure extractors for dataform release and workflow config to extract name only as tf expects

--- config/cluster/dataform/config.go
+++ config/cluster/dataform/config.go
@@ -12,22 +12,25 @@ func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("google_dataform_repository", func(r *config.Resource) {
 		r.MarkAsRequired("region")
 	})
+	// needs full resource path
 	p.AddResourceConfigurator("google_dataform_repository_iam_member", func(r *config.Resource) {
 		r.References["repository"] = config.Reference{
 			TerraformName: "google_dataform_repository",
 			Extractor:     "github.com/crossplane/upjet/v2/pkg/resource.ExtractParamPath(\"id\", true)",
 		}
 	})
+	// needs just the repo name, fails on full path
 	p.AddResourceConfigurator("google_dataform_repository_release_config", func(r *config.Resource) {
 		r.References["repository"] = config.Reference{
 			TerraformName: "google_dataform_repository",
-			Extractor:     "github.com/crossplane/upjet/v2/pkg/resource.ExtractParamPath(\"id\", true)",
+			Extractor:     "github.com/crossplane/upjet/v2/pkg/resource.ExtractParamPath(\"name\", false)",
 		}
 	})
+	// needs just the repo name, fails on full path
 	p.AddResourceConfigurator("google_dataform_repository_workflow_config", func(r *config.Resource) {
 		r.References["repository"] = config.Reference{
 			TerraformName: "google_dataform_repository",
-			Extractor:     "github.com/crossplane/upjet/v2/pkg/resource.ExtractParamPath(\"id\", true)",
+			Extractor:     "github.com/crossplane/upjet/v2/pkg/resource.ExtractParamPath(\"name\", false)",
 		}
 	})
 }
